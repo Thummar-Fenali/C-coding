23481f9cb72e080fb01b7766c0136ed550a60bbd
Thummar-Fenali <thummarfenali@gmail.com>
2024-06-10 13:42:35 +0530

    10/6/24github

---
+++ ch -4/EXPLICIT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a=34;
+float pi =3.14;
+clrscr();
+printf("A\t: %d\n",a);
+printf("A\t: %f",(float)a);
+printf("\n \n");
+printf("Pi\t: %2f\n",pi);
+printf("Pi\t: %d",
+(int)pi);
+getch();
+}
---
+++ ch -4/IMPLICIT.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int a=65;
+  char ch='f';
+  clrscr();
+  printf("A\t:%d\n",a);
+  printf("A\t:%ch",a);
+  printf("\n \n");
+  printf("ch\t:%c\n",ch);
+  printf("ch\t: %d",ch);
+  getch();
+  }
